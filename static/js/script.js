18dd9daac61e575b670a8339afe0b22e2b7de80d
IamAnkitBhardwaj <ankitk221706@gmail.com>
2025-10-05 00:29:14 +0530

    Force add static folder for deployment

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// static/js/script.js
+
+// Initialize Animate on Scroll
+AOS.init({
+    duration: 800, // animation duration in milliseconds
+    once: true, // whether animation should happen only once - while scrolling down
+});
