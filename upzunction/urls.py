7fae496dae4b08ff9287adf929eab156883afa88
IamAnkitBhardwaj <ankitk221706@gmail.com>
2025-10-05 23:26:59 +0530

    Security Cleanup: Remove temporary superuser creation artifacts

--- upzunction/urls.py
+++ upzunction/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     
     # This correctly imports all the pre-built auth URLs like login/logout.
     path('accounts/', include('django.contrib.auth.urls')),
-    path('create-superuser-a9b8c7d6e5f4g3h2i1j0k/', views.create_superuser_temp_view, name='create_superuser_temp'),
+   
     # This correctly imports all of your app's URLs from social/urls.py,
     # including the new OTP registration URLs.
     path('', include('social.urls')),
