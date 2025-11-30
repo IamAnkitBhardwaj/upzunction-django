ddbd972167ac6eeb6ab0a0f52912b75c3bd714cd
IamAnkitBhardwaj <ankitk221706@gmail.com>
2025-11-30 20:45:36 +0530

    Retry adding secret admin URL

--- upzunction/urls.py
+++ upzunction/urls.py
@@ -9,11 +9,14 @@ from django.urls import path, include
 from social import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('create-admin-secret-page/', views.create_superuser_temp_view),
     # This correctly imports all the pre-built auth URLs like login/logout.
     path('accounts/', include('django.contrib.auth.urls')),
    
     # This correctly imports all of your app's URLs from social/urls.py,
     # including the new OTP registration URLs.
     path('', include('social.urls')),
+
+
+    
 ]
